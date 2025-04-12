2b53d63499b2f613e473e7a46174a1402dd3c7c9
estudiante103 <estudiante103@103-17.unicamacho.co>
2025-04-11 19:39:07 -0500

    se reralizo la clase criatura

--- src/parcial2/Criatura.java
+++ src/parcial2/Criatura.java
@@ -10,5 +10,29 @@ package parcial2;
  * @author estudiante103
  */
 public class Criatura {
+    protected String nombre;
+    protected int salud;
+    protected int fuerza;
+    protected Arma arma;
     
+    public criatura(String nombre, int nombre, int salud, int fuerza) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.fuerza = fuerza;
+    }
+    
+    public void equiparArma(Arma arma) {
+        this.arma = arma;
+    }
+    public void desequiparArma() { 
+        this.arma = null;
+    }
+    
+    public boolean estaViva() { 
+        return salud >0;
+    }
+    
+    public abstract void atacar(Criatura objetivo);
+    public abstract void defender(int daño);
 }
+
